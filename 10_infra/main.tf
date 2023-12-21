ed4b64f04628ecd7989581223914b9c4c7911577
mouse4562 <mmshinoda1970@gmail.com>
2023-12-21 17:00:48 +0900

    S3バケット名追加

--- 10_infra/main.tf
+++ 10_infra/main.tf
@@ -10,8 +10,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket  = "{YOUR-S3-BUCKET-NAME}"
-    key     = "{YOUR-S3-BUCKET-KEY}"
+    bucket  = "udemy.terraform.s3"
+    key     = "terraform.udemy.test.tfstate"
     region  = "ap-northeast-1"
     profile = "terraform"
   }
